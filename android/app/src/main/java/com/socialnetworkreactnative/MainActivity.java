2ef78110e5d1c1b3b7481eddef7758a6584e4120
HuyND11 <huy.nguyen23@student.passerellesnumeriques.org>
2022-12-29 15:22:28 +0700

    Install react navigation

--- android/app/src/main/java/com/socialnetworkreactnative/MainActivity.java
+++ android/app/src/main/java/com/socialnetworkreactnative/MainActivity.java
@@ -3,6 +3,7 @@ package com.socialnetworkreactnative;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "SocialNetworkReactNative";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
