132da74a1dffc30c1324a9a2be7a24334d635e79
HuyND11 <huy.nguyen23@student.passerellesnumeriques.org>
2023-01-08 22:15:18 +0700

    Implement header PNVO

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,12 +1,11 @@
 import {Text, View} from 'react-native';
-// import Header from '../../components/Header';
+import Header from '../../components/Header';
 import Post from '../../components/Post';
 
 const Home = () => {
   return (
     <View>
-      {/* <Header /> */}
-      <Text>Home page</Text>
+      <Header />
       <Post />
     </View>
   );
