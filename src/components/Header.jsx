a68cd66c12db056e965570b38463cfe55da1080f
HuyND11 <huy.nguyen23@student.passerellesnumeriques.org>
2023-01-09 19:08:51 +0700

    comment post UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    paddingHorizontal: 8,
+    // paddingHorizontal: 8,
     paddingVertical: 5,
   },
 
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,14 +1,22 @@
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
+import Comment from '../../components/comment/Comment';
 import Header from '../../components/Header';
 import Post from '../../components/Post';
 
 const Home = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Header />
       <Post />
+      <Comment />
     </View>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+  },
+});
