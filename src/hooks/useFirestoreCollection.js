e75dd017a4b6275f2d3cf8b4d0252a278f3c17a3
HuyND11 <huy.nguyen23@student.passerellesnumeriques.org>
2023-01-09 21:32:02 +0700

    Fix bug useFirestoreCollection

--- src/hooks/useFirestoreCollection.js
+++ src/hooks/useFirestoreCollection.js
@@ -1,7 +1,123 @@
 import {useEffect, useState} from 'react';
 import {firestore} from '@react-native-firebase/firestore';
 
-function useFirestoreCollection(collection) {
+// function useFirestoreCollection(collection, pageSize, page) {
+//   const [data, setData] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+//   const [query, setQuery] = useState(null);
+//   const [queryLoading, setQueryLoading] = useState(false);
+//   const [queryError, setQueryError] = useState(null);
+
+//   useEffect(() => {
+//     let unsubscribe;
+//     if (query) {
+//       setQueryLoading(true);
+//       unsubscribe = query.onSnapshot(
+//         querySnapshot => {
+//           const data = [];
+//           querySnapshot.forEach(doc => {
+//             data.push({
+//               id: doc.id,
+//               ...doc.data(),
+//             });
+//           });
+//           setData(data);
+//           setQueryLoading(false);
+//         },
+//         error => {
+//           setQueryError(error);
+//           setQueryLoading(false);
+//         },
+//       );
+//     } else {
+//       setLoading(true);
+//       unsubscribe = collection
+//         .limit(pageSize)
+//         .offset(page * pageSize)
+//         .onSnapshot(
+//           collectionSnapshot => {
+//             const data = [];
+//             collectionSnapshot.forEach(doc => {
+//               data.push({
+//                 id: doc.id,
+//                 ...doc.data(),
+//               });
+//             });
+//             setData(data);
+//             setLoading(false);
+//           },
+//           error => {
+//             setError(error);
+//             setLoading(false);
+//           },
+//         );
+//     }
+//     return () => unsubscribe();
+//   }, [collection, query, pageSize, page]);
+
+//   function refresh() {
+//     if (query) {
+//       setQueryLoading(true);
+//       query.get().then(
+//         querySnapshot => {
+//           const data = [];
+//           querySnapshot.forEach(doc => {
+//             data.push({
+//               id: doc.id,
+//               ...doc.data(),
+//             });
+//           });
+//           setData(data);
+//           setQueryLoading(false);
+//         },
+//         error => {
+//           setQueryError(error);
+//           setQueryLoading(false);
+//         },
+//       );
+//     } else {
+//       setLoading(true);
+//       collection
+//         .limit(pageSize)
+//         .offset(page * pageSize)
+//         .get()
+//         .then(
+//           collectionSnapshot => {
+//             const data = [];
+//             collectionSnapshot.forEach(doc => {
+//               data.push({
+//                 id: doc.id,
+//                 ...doc.data(),
+//               });
+//             });
+//             setData(data);
+//             setLoading(false);
+//           },
+//           error => {
+//             setError(error);
+//             setLoading(false);
+//           },
+//         );
+//     }
+//   }
+
+//   function setCollectionQuery(newQuery) {
+//     setQuery(newQuery);
+//   }
+
+//   return {
+//     data,
+//     loading,
+//     error,
+//     queryLoading,
+//     queryError,
+//     refresh,
+//     setCollectionQuery,
+//   };
+// }
+
+function useFirestoreCollection(collection, pageSize, page) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -31,27 +147,29 @@ function useFirestoreCollection(collection) {
         },
       );
     } else {
-      setLoading(true);
-      unsubscribe = collection.onSnapshot(
-        collectionSnapshot => {
-          const data = [];
-          collectionSnapshot.forEach(doc => {
-            data.push({
-              id: doc.id,
-              ...doc.data(),
+      unsubscribe = collection
+        .limit(pageSize)
+        // .offset(page * pageSize)
+        .onSnapshot(
+          collectionSnapshot => {
+            const data = [];
+            collectionSnapshot.forEach(doc => {
+              data.push({
+                id: doc.id,
+                ...doc.data(),
+              });
             });
-          });
-          setData(data);
-          setLoading(false);
-        },
-        error => {
-          setError(error);
-          setLoading(false);
-        },
-      );
+            setData(data);
+            setLoading(false);
+          },
+          error => {
+            setError(error);
+            setLoading(false);
+          },
+        );
     }
     return () => unsubscribe();
-  }, [collection, query]);
+  }, [collection, query, pageSize, page]);
 
   function refresh() {
     if (query) {
@@ -74,24 +192,27 @@ function useFirestoreCollection(collection) {
         },
       );
     } else {
-      setLoading(true);
-      collection.get().then(
-        collectionSnapshot => {
-          const data = [];
-          collectionSnapshot.forEach(doc => {
-            data.push({
-              id: doc.id,
-              ...doc.data(),
+      collection
+        .limit(pageSize)
+        // .offset(page * pageSize)
+        .get()
+        .then(
+          collectionSnapshot => {
+            const data = [];
+            collectionSnapshot.forEach(doc => {
+              data.push({
+                id: doc.id,
+                ...doc.data(),
+              });
             });
-          });
-          setData(data);
-          setLoading(false);
-        },
-        error => {
-          setError(error);
-          setLoading(false);
-        },
-      );
+            setData(data);
+            setLoading(false);
+          },
+          error => {
+            setError(error);
+            setLoading(false);
+          },
+        );
     }
   }
 
